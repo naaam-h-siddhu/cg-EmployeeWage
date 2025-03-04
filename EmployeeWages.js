d8a3532ad1b14875fd0112fbc4166d40b14b1708
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-03-05 00:03:03 +0530

    [Sudhakar] Add. Added program to find the first occurrence of full time wage

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -92,3 +92,9 @@ function fulltimeWage(dailyWage){
 let fullDayWageArr = mapDayWithWageArr.filter(fulltimeWage)
 console.log("UC7C - Daily Wage Filter When Fulltime Wage Earned")
 console.log(fullDayWageArr)
+
+//UC 7D
+function findFulltimeWage(dailyWage){
+  return dailyWage.includes("160")
+}
+console.log("UC 7D - First time Fulltime wage was earned on Day: "+mapDayWithWageArr.find(fulltimeWage))
